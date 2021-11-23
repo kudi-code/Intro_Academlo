af87c908cf096ea6200e9ad6f198858fe7a52257
Kevin Casas <kevinsal.casas@gmail.com>
2021-11-22 22:24:19 -0600

    Class 5

--- Introduccion_Academlo.js
+++ Introduccion_Academlo.js
@@ -1,20 +1,20 @@
 //----------------------Clase 1------------------------//
 clear() //Limpia la consola
+
 //-----------------Clase 2: Variables------------------//
 //= operador de asignación 
 //Variables de tipo numérico
 var precioProducto1 = 10
 var precioProducto2 = 20
 var total = precioProducto1 + precioProducto2
-
 //Variables de tipo string
 var nombre= "Erik" 
 var apellido ="Pérez"
 // nombre + apellido = concatenación
-
 var n1="50"
 var n2 ="20"
 //n1+n2 = 5020 concactenado
+
 //----------------------Clase 3: Arreglos---------------//
 var numero=60
 var nombre = "Erik"
@@ -23,10 +23,13 @@ var edades = [60,45,80,90]
 var nombres =["Erik","Luis","Pedro"]
 //Array con array
 var arreglo =["Erik", 50, [24,20], 4]
+
 //-----Clase 4: Accediendo a los valores de un array----//
 //Valores de un Array inicia en 0
 edades[0] //Del 0 al 3
 nombres[1] //"Luis"
 var texto = "En el arreglo Nombres tenemos a: " + nombres[0] + " , "+nombres[1] + " y "+nombres[2]
 
-
+//-----------Clase 5: Operando arrays-------------------//
+var suma= edades[0] + edades[1]
+var resta= edades[0] + edades[3]
