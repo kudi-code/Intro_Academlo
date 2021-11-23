ca7ce8a71df04428e9e2952928f3cd45e904104e
Kevin Casas <kevinsal.casas@gmail.com>
2021-11-22 21:53:00 -0600

    Quiz 1

---
+++ All-Quiz.js
@@ -0,0 +1,14 @@
+//-----------------Quiz 1------------------//
+
+//Crear variables para guardar los siguientes datos de una persona:
+var Nombre
+var Apellidos
+var Edad
+var Ciudad
+
+//Asignar valores a las variables creadas.
+
+Nombre= "Kevin"
+Apellidos= "Salvador Casas"
+Edad = 24
+Ciudad= "Oaxaca de Juárez"
