92ede409a30986c4e5b46becb2065a420c5b5985
Kevin Casas <kevinsal.casas@gmail.com>
2021-11-22 21:52:29 -0600

    Quiz 1 and Class 2

--- Introduccion_Academlo.js
+++ Introduccion_Academlo.js
@@ -1,3 +1,18 @@
-//-----------------Clase 1------------------//
+//----------------------Clase 1------------------------//
 clear() //Limpia la consola
-//-----------------Clase 2------------------//
+//-----------------Clase 2: Variables------------------//
+//= operador de asignación 
+//Variables de tipo numérico
+var precioProducto1 = 10
+var precioProducto2 = 20
+var total = precioProducto1 + precioProducto2
+
+//Variables de tipo string
+var nombre= "Erik" 
+var apellodo ="Pérez"
+// nombre + apellido = concatenación
+
+var n1="50"
+var n2 ="20"
+//n1+n2 = 5020 concactenado
+
