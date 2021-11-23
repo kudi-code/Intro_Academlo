acd82f50b7fa3beaa3ae481d984329e39df5c460
Kevin Casas <kevinsal.casas@gmail.com>
2021-11-22 22:26:22 -0600

    Quiz 2

--- All-Quiz.js
+++ All-Quiz.js
@@ -14,3 +14,7 @@ Edad = 24
 Ciudad= "Oaxaca de Juárez"
 
 //-----------------Quiz 2------------------//
+//Crear un array con 5 valores numericos y sumar todos esos valores accediendolos mediante las posiciones
+var precios = [5,10,20,40]
+var sumatoria = precios[0]+ precios[3]+precios[2]
+
